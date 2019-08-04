cfae5ca761c046403e1432435ba9700731aef64c
KokosnussDEV <kcvw@icloud.com>
2019-08-04 11:27:09 +0200

    Update police_chase_POLICE.ino

--- police_chase_POLICE/police_chase_POLICE.ino
+++ police_chase_POLICE/police_chase_POLICE.ino
@@ -19,7 +19,7 @@ void loop() {
     robot.driveStraight(0);
     robot.driveLoop();
     while (true) {
-      if (robot.distanceFront() > 30 || robot.distanceFront() < 0) {
+      //if (robot.distanceFront() > 30 || robot.distanceFront() < 0) {
         robot.turn(0,0);
         if (digitalRead(LINE_LEFT_PIN) == HIGH) {
           robot.driveStraight(200);
@@ -32,20 +32,19 @@ void loop() {
           delay(1000);
           robot.driveStraight(0);
           if (digitalRead(LINE_LEFT_PIN) == LOW) {
-            robot.turn(-45, 200);
             robot.driveStraight(200);
             delay(1000);
             robot.driveStraight(0);
           }
         }
-      } else {
+      /*} else {
         robot.driveStraight(-50);
         robot.driveLoop();
         delay(100);
         robot.driveStraight(0);
         robot.driveLoop();
         robot.turn(360, 100);
-      }
+      }*/
       
     }
   }
